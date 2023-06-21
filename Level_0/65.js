3bac5d8dfec39059f2b622b749daccac8aca7bc1
SeungKu_Kang <kangsg26@naver.com>
2023-06-22 01:29:24 +0900

    * [Level 0][Day 13 리스트(배열)] 문제해결
    - 61 ~ 65번 문제해결

---
+++ Level_0/65.js
@@ -0,0 +1,14 @@
+/**
+ * [n개 간격의 원소들]
+ * 
+ * 정수 리스트 num_list와 정수 n이 주어질 때, 
+ * num_list의 첫 번째 원소부터 마지막 원소까지 n개 간격으로 저장되어있는 
+ * 원소들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+ */
+
+const solution = (num_list, n) => num_list.filter((_, i) => !(i % n));
+
+console.time();
+console.log(solution([4, 2, 6, 1, 7, 6], 2));	// [4, 6, 7]
+console.log(solution([4, 2, 6, 1, 7, 6], 4));	// [4, 7]
+console.timeEnd();
