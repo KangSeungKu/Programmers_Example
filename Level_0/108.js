1d70a5b0dadcb59dc024aa8e646102c246d404a2
SeungKu_Kang <kangsg26@naver.com>
2023-06-26 21:25:07 +0900

    * [Level 0][Day 22 함수(메서드), 조건문 활용]
    - 106 ~ 110번 문제해결

---
+++ Level_0/108.js
@@ -0,0 +1,12 @@
+/**
+ * [문자열로 변환]
+ * 
+ * 정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+ */
+
+const solution = (n) => n + "";
+
+console.time();
+console.log(solution(123));	// "123"
+console.log(solution(2573));	// "2573"
+console.timeEnd();
